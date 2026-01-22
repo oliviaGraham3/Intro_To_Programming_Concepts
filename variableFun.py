437e49b56bf777c7d1f871a561def852303fe3e9
oliviaGraham3 <people0person0thing@gmail.com>
2026-01-22 10:35:13 -0500

    Variable Fun Assignment

---
+++ variableFun.py
@@ -0,0 +1,19 @@
+questionOne = 5 * 6
+print("The answer to your query is: ", questionOne , "\n")
+
+questionTwo = 8 / 3
+print("The answer to your query is: ", questionTwo , "\n")
+
+questionThree = 8 % 3 #shhh I don't know if this is the right way but it gets the right answer
+print("The answer to your query is: ", questionThree , "\n")
+
+questionFour = 8 % 3
+print("The answer to your query is: ", questionFour , "\n")
+
+questionFive = 4200000 + 23090
+print("The answer to your query is: ", questionFive , "\n")
+
+questionSix = 4200000 - 7633
+print("The answer to your query is: ", questionSix , "\n")
+
+print("Olivia Graham")
