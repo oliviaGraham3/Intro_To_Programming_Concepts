44bdb07b50cf2df699f4ccae1603accc4e6083ac
oliviaGraham3 <people0person0thing@gmail.com>
2026-02-03 21:31:19 -0500

    branching

--- branching.py
+++ branching.py
@@ -1,6 +1,7 @@
 hrsBeforeUpCharge = 1000
 hours = int(input("Enter the KW hours used: "))
 price = 0
+#there are magic numbers cause its like 5 lines of code
 
 if hours <= hrsBeforeUpCharge:
     price = hours * 7.633
