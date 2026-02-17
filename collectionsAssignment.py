f08b799c27cbd128a9a0cc5e5f1f752d1c9d2382
oliviaGraham3 <people0person0thing@gmail.com>
2026-02-17 16:34:44 -0500

    collections

--- collectionsAssignment.py
+++ collectionsAssignment.py
@@ -19,22 +19,25 @@ gradeList.append(grade5)
 
 avg = (gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4])/ len(gradeList)
 
-if avg < 0 or avg > 100:
-    letterGrade = "OOR"
-elif avg >= 90:
-    letterGrade = "A"
-elif avg >= 80:
-    letterGrade = "B"
-elif avg >= 70:
-    letterGrade = "C"
-elif avg >= 60:
-    letterGrade = "D"
-else:
-    letterGrade = "F"
+def gradePrint(avg):
+    if avg < 0 or avg > 100:
+        letterGrade = "OOR"
+    elif avg >= 90:
+        letterGrade = "A"
+    elif avg >= 80:
+        letterGrade = "B"
+    elif avg >= 70:
+        letterGrade = "C"
+    elif avg >= 60:
+        letterGrade = "D"
+    else:
+        letterGrade = "F"
 
-if letterGrade != "OOR":
-    print(studentName)
-    print("Average: " , avg)
-    print("Letter Grade: " , letterGrade)
-elif letterGrade == "OOR":
-    print("Your average grade was out of range, please reload and try again")
+    if letterGrade != "OOR":
+        print(studentName)
+        print("Average: " , avg)
+        print("Letter Grade: " , letterGrade)
+    elif letterGrade == "OOR":
+        print("Your average grade was out of range, please reload and try again")
+
+gradePrint(avg)
